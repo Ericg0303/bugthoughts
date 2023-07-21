b97ec407156fdc0a47a9cbb1cd3b059053eafbb0
Eric Garcia <ericg0303@gmail.com>
2023-07-20 23:41:17 -0500

    Cofigure

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          A Special Gift
         </a>
       </header>
     </div>
